07803c738a772e427af180f982d5c98219443f60
akjfal <osydooo@gmail.com>
2020-12-30 01:47:22 +0900

    readme

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { useCachedResources } from "./src/hooks";
-import { Loading } from "./src/screens";
-import EnrollNavigation from "./src/navigation/EnrollNavigation";
-import ProviderWrapper from "./Provider";
-import * as font from "expo-font";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useCachedResources } from './src/hooks';
+import { Loading } from './src/screens';
+import EnrollNavigation from './src/navigation/EnrollNavigation';
+import ProviderWrapper from './Provider';
+import * as font from 'expo-font';
 
 export default function App() {
   const [loaded] = font.useFonts({
-    NanumBarunGothic: require("./src/assets/fonts/NanumBarunGothic.ttf"),
-    NanumBarunGothicBold: require("./src/assets/fonts/NanumBarunGothicBold.ttf"),
-    snapsPinoBold: require("./src/assets/fonts/snaps_Pino-Bold.ttf"),
-    snapsPino: require("./src/assets/fonts/snaps_Pino.ttf"),
+    NanumBarunGothic: require('./src/assets/fonts/NanumBarunGothic.ttf'),
+    NanumBarunGothicBold: require('./src/assets/fonts/NanumBarunGothicBold.ttf'),
+    snapsPinoBold: require('./src/assets/fonts/snaps_Pino-Bold.ttf'),
+    snapsPino: require('./src/assets/fonts/snaps_Pino.ttf'),
   });
 
   const isLoadingComplete = useCachedResources();
-  console.log(loaded);
   if (!loaded) {
     return (
       <ProviderWrapper>
@@ -36,12 +35,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   loading: {
-    width: "100%",
-    height: "100%",
+    width: '100%',
+    height: '100%',
   },
 });
--- src/hooks/useOcr.js
+++ src/hooks/useOcr.js
@@ -8,11 +8,10 @@ import { setProduct } from '../reducers/Product';
 const quickstart = async (image) => {
   let today = new Date();
   const data = await axios({
-    url:
-      'https://914de598614046ba8ed9926e156f0a31.apigw.ntruss.com/custom/v1/4048/f59721ea1f9fbd7439e43de741937b01a938480cfad748e00dcbee7807d1c016/general',
+    url: process.env.REACT_APP_OCRURL,
     method: 'post',
     headers: {
-      'X-OCR-SECRET': 'ZlR6Y1BLUVNQTllDZndsenlSSWRQU0xWRlRFV0tKRWY=',
+      'X-OCR-SECRET': process.env.REACT_APP_OCRSECRET,
       'Content-Type': 'application/json; charset=utf-8',
     },
     data: {
@@ -23,14 +22,12 @@ const quickstart = async (image) => {
       images: [{ format: 'png', data: image, name: '식품' }],
     },
   });
-  // const data = `제품명 치즈초코파이 식품유형 초콜릿 내용량 384g 업소명 및 소재지 롯데제과(주) 경남 양산시 양산대로 1158 유통기한 측면표기일까지(연.월.일) 내포장재질 폴리프로필렌 품목보고번호 19780614009442 밀가루(밀;미국산),탕,물엿,쇼트닝(가공유지(부분경화유;팜스터아린유(말레이시아산),동물성유지(호주산)),식물성지(부분경화유(말레이시아산); 원재료명 팜유,팜핵유),혼합분유,코코아분말(인도네시0산),D-소비톨액,글리서린,체다치즈분말(덴마크산),주정0.63%,유당,기타가공품,포도당,젤라틴,전란액,정제소금,산도조절제I,합성향료(치즈형,밀크향,바닐라향),산도절제Ⅱ,구연산,코코아마스(코코아빈;가나산),유화제I,신도조절제II,홍화황색소,유화제II,혼합제제(유화제,아라비아검,산도조절제),바닐린,잔탄검밀,쇠고기,대두,우유,돼지고기,계란 함유`;
   return data;
 };
 
 const sendServer = async (text, vegan) => {
   const data = await axios({
-    // url: 'https://capstonvegan.herokuapp.com/api/sendText',
-    url: 'http://192.168.43.38:8080/api/sendText',
+    url: process.env.REACT_APP_BASEURL,
     method: 'post',
     data: {
       text: text,
@@ -53,18 +50,15 @@ export default function useOcr(image, navigation, nextNav) {
 
   React.useEffect(() => {
     if (ocr.pending || checkVegan.pending) {
-      // setLoading(true);
     }
   }, [ocr.pending, checkVegan.pending]);
 
   React.useEffect(() => {
     if (ocr.fulfilled) {
-      console.log('ocr---------------success-------------');
       let text = '';
       ocr.data.data.images[0].fields.forEach((value, index) => {
         text += `${value.inferText} `;
       });
-      console.log(text);
       postCheckVegan(text, user.vegan - 1);
       textEl.current = text;
     }
@@ -72,20 +66,6 @@ export default function useOcr(image, navigation, nextNav) {
 
   React.useEffect(() => {
     if (checkVegan.fulfilled) {
-      console.log('server---------------success-------------');
-      console.log('---------------vegan-------------');
-      console.log(checkVegan.data.data.vegan);
-      console.log('---------------type-------------');
-      console.log(checkVegan.data.data.type);
-      console.log('---------------title-------------');
-      console.log(checkVegan.data.data.title);
-      console.log('---------------not_match-------------');
-      console.log(checkVegan.data.data.not_match);
-      console.log('---------------desc-------------');
-      console.log(textEl.current);
-      console.log('---------------substitution-------------');
-      console.log(checkVegan.data.data.substitution);
-
       const product = {
         vegan: checkVegan.data.data.vegan,
         cover: `data:image/png;base64,${image}`,
@@ -115,16 +95,12 @@ export default function useOcr(image, navigation, nextNav) {
 
   React.useEffect(() => {
     if (ocr.rejected) {
-      console.log('ocr---------------Error-------------');
-      console.log(ocr.error);
       setError(true);
     }
   }, [ocr.rejected]);
 
   React.useEffect(() => {
     if (checkVegan.rejected) {
-      console.log('server---------------Error-------------');
-      console.log(checkVegan.error);
       setError(true);
     }
   }, [checkVegan.rejected]);
